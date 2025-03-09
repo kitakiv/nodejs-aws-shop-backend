750eff6229b19af849d8c11e5dc3c3ff989dc967
Viktoriya Nykytenko <kitakiv@gmail.com>
2025-03-09 13:22:06 +0200

    feat: write test for import service

--- cdk-import-service/test/cdk-import-service.test.ts
+++ cdk-import-service/test/cdk-import-service.test.ts
@@ -1,17 +1,87 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkImportService from '../lib/cdk-import-service-stack';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { handler } from '../lambda/importProducts';
+import { StatusCode, StatusCodeMessage } from '../lambda/functions/constants';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-import-service-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkImportService.CdkImportServiceStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+jest.mock('../lambda/functions/presigned', () => ({
+  createPresignedUrlWithClient: jest.fn().mockImplementation(() => 
+    Promise.resolve('https://mock-signed-url.com')
+  )
+}));
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe('importProducts handler', () => {
+  const mockHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should return signed URL when valid CSV filename is provided', async () => {
+    const mockEvent = {
+        queryStringParameters: {
+            name: 'test.csv'
+        }
+    } as unknown as APIGatewayProxyEvent;
+    const response = await handler(mockEvent);
+    expect(response.statusCode).toBe(StatusCode.SUCCESS);
+    expect(JSON.parse(response.body)).toBe('https://mock-signed-url.com');
+  });
+
+  test('should return BAD_REQUEST when filename is missing', async () => {
+    const mockEvent = {
+      queryStringParameters: {}
+    } as APIGatewayProxyEvent;
+    const response = await handler(mockEvent);
+    expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
+    expect(JSON.parse(response.body)).toEqual({
+      message: StatusCodeMessage.BAD_REQUEST
+    });
+  });
+
+  test('should return BAD_REQUEST when file is not CSV', async () => {
+    const mockEvent = {
+        queryStringParameters: {
+            name: 'test.txt'
+        }
+    } as unknown as APIGatewayProxyEvent;
+
+    const response = await handler(mockEvent);
+
+    expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
+    expect(JSON.parse(response.body)).toEqual({
+      message: StatusCodeMessage.BAD_REQUEST
+    });
+  });
+
+  test('should return INTERNAL_SERVER_ERROR when createPresignedUrlWithClient fails', async () => {
+    // Arrange
+    const mockEvent = {
+        queryStringParameters: {
+            name: 'test.csv'
+        }
+    } as unknown as APIGatewayProxyEvent;
+    const { createPresignedUrlWithClient } = require('../lambda/functions/presigned');
+    createPresignedUrlWithClient.mockImplementationOnce(() =>
+      Promise.reject(new Error('Failed to create signed URL'))
+    );
+    const response = await handler(mockEvent);
+    expect(response.statusCode).toBe(StatusCode.INTERNAL_SERVER_ERROR);
+    expect(JSON.parse(response.body)).toEqual({
+      message: 'Internal server error'
+    });
+  });
+
+  test('should handle event with null queryStringParameters', async () => {
+    const mockEvent = {
+      queryStringParameters: null
+    } as APIGatewayProxyEvent;
+    const response = await handler(mockEvent);
+    expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
+    expect(JSON.parse(response.body)).toEqual({
+      message: StatusCodeMessage.BAD_REQUEST
+    });
+  });
 });
+
